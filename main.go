68f1eec417e0d2f336a41f1b4d004fd55a6805ed
binary cat <dogedoge61@gmail.com>
2020-08-22 23:06:01 -0400

    aaaaaaa

--- main.go
+++ main.go
@@ -1,10 +1,44 @@
 // patmatch is a tool for pattern matching in bash scripts
 package main
 
-import "os"
+import (
+	"os"
+	"os/exec"
+	"log"
+)
 
 const Shell = "/bin/bash"
 
+//go:generate peg grammar.peg
+
 func main() {
-	exec.Setenv("foo","1")
+	src := `echo test
+`
+	p := Prog{Cmd:&exec.Cmd{
+		Path:Shell,
+		Stdout:os.Stdout,
+	},Buffer:src}
+	var err error
+	p.In, err = p.Cmd.StdinPipe()
+	fatal(err)
+	logln("starting shell")
+	err = p.Cmd.Start()	
+	fatal(err)
+	logln("initalizing parser")
+	p.Init()
+	fatal(err)
+	logln("parsing")
+	err = p.Parse()
+	fatal(err)
+	logln("executing")
+	p.Execute()
+	logln("done")
+}
+
+func fatal(err error) {
+	if err != nil {panic(err)}
+} 
+
+func logln(args ...interface{}) {
+	log.Println(args...)
 }
